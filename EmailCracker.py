b62fc3629496eefe2f7473d5176e1df808468673
Steven <143050564+HAKR702@users.noreply.github.com>
2025-04-10 23:23:14 -0400

    EmailCracker.py

--- EmailCracker.py
+++ EmailCracker.py
@@ -24,7 +24,7 @@ print(Prompt)
 import smtplib
 import time 
 import logging 
- 
+import os
 class bcolors:
     OK = '\033[92m'
     FAIL = '\033[91m'
@@ -78,7 +78,7 @@ except Exception as e:
 print (bcolors.BOLD + "HackBitGod Email Cracker" + bcolors.ENDC)
 print (bcolors.BOLD + "TRYING WITH PASSWORDS IN: psw.list" + bcolors.ENDC)
  
-user = raw_input("Enter The Victim's Email Address: ")
+user = input("Enter The Victim's Email Address: ")
 passwfile = "psw.list"
 passwfile = open(passwfile, "r")
  
